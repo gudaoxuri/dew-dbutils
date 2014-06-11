823b7492772a7192bccf4e810ba77002f1e8e75e
孤岛旭日 <net@sunisle.org>
2014-06-11 16:02:27 +0800

    Add null check when load multi ds.

--- src/main/java/com/ecfront/easybi/dbutils/inner/DSLoader.java
+++ src/main/java/com/ecfront/easybi/dbutils/inner/DSLoader.java
@@ -49,7 +49,7 @@ public class DSLoader {
             } else {
                 Class.forName(dsEntity.driver).newInstance();
                 connection = DriverManager.getConnection(dsEntity.url, dsEntity.userName, dsEntity.password);
-                if(null== connection){
+                if (null == connection) {
                     logger.error("Connection can't create.");
                 }
             }
@@ -75,22 +75,54 @@ public class DSLoader {
                 if (null != res) {
                     DSEntity dsEntity = new DSEntity();
                     dsEntity.flag = res.get(ConfigContainer.FLAG_CODE.toUpperCase()).toString();
-                    dsEntity.poolSupport = Boolean.valueOf(res.get(ConfigContainer.FLAG_POOL_SUPPORT.toUpperCase()).toString());
-                    dsEntity.monitor = Boolean.valueOf(res.get(ConfigContainer.FLAG_MONITOR.toUpperCase()).toString());
+                    if (res.get(ConfigContainer.FLAG_POOL_SUPPORT.toUpperCase()) != null) {
+                        dsEntity.poolSupport = Boolean.valueOf(res.get(ConfigContainer.FLAG_POOL_SUPPORT.toUpperCase()).toString());
+                    } else {
+                        dsEntity.poolSupport = true;
+                    }
+                    if (res.get(ConfigContainer.FLAG_MONITOR.toUpperCase()) != null) {
+                        dsEntity.monitor = Boolean.valueOf(res.get(ConfigContainer.FLAG_MONITOR.toUpperCase()).toString());
+                    } else {
+                        dsEntity.monitor = true;
+                    }
                     dsEntity.url = res.get(ConfigContainer.FLAG_URL.toUpperCase()).toString();
                     dsEntity.driver = res.get(ConfigContainer.FLAG_DRIVER.toUpperCase()).toString();
-                    dsEntity.userName = res.get(ConfigContainer.FLAG_USERNAME.toUpperCase()).toString();
-                    dsEntity.password = res.get(ConfigContainer.FLAG_PASSWORD.toUpperCase()).toString();
-                    dsEntity.defaultAutoCommit = Boolean.valueOf(res.get(ConfigContainer.FLAG_DEFAULT_AUTO_COMMIT.toUpperCase()).toString());
-                    dsEntity.initialSize = Integer.valueOf(res.get(ConfigContainer.FLAG_INITIAL_SIZE.toUpperCase()).toString());
-                    dsEntity.maxActive = Integer.valueOf(res.get(ConfigContainer.FLAG_MAX_ACTIVE.toUpperCase()).toString());
-                    dsEntity.minIdle = Integer.valueOf(res.get(ConfigContainer.FLAG_MIN_IDLE.toUpperCase()).toString());
-                    dsEntity.maxIdle = Integer.valueOf(res.get(ConfigContainer.FLAG_MAX_IDLE.toUpperCase()).toString());
-                    dsEntity.maxWait = Integer.valueOf(res.get(ConfigContainer.FLAG_MAX_WAIT.toUpperCase()).toString());
-                    dsEntity.removeAbandoned = Boolean.valueOf(res.get(ConfigContainer.FLAG_REMOVE_ABANDONED.toUpperCase()).toString());
-                    dsEntity.removeAbandonedTimeoutMillis = Integer.valueOf(res.get(ConfigContainer.FLAG_REMOVE_ABANDONED_TIMEOUT.toUpperCase()).toString());
-                    dsEntity.timeBetweenEvictionRunsMillis = Integer.valueOf(res.get(ConfigContainer.FLAG_TIME_BETWEEN_EVICTION_RUMS.toUpperCase()).toString());
-                    dsEntity.minEvictableIdleTimeMillis = Integer.valueOf(res.get(ConfigContainer.FLAG_MIN_EVICTABLE_IDLE_TIME.toUpperCase()).toString());
+                    if (res.get(ConfigContainer.FLAG_USERNAME.toUpperCase()) != null) {
+                        dsEntity.userName = res.get(ConfigContainer.FLAG_USERNAME.toUpperCase()).toString();
+                    }
+                    if (res.get(ConfigContainer.FLAG_PASSWORD.toUpperCase()) != null) {
+                        dsEntity.password = res.get(ConfigContainer.FLAG_PASSWORD.toUpperCase()).toString();
+                    }
+                    if (res.get(ConfigContainer.FLAG_DEFAULT_AUTO_COMMIT.toUpperCase()) != null) {
+                        dsEntity.defaultAutoCommit = Boolean.valueOf(res.get(ConfigContainer.FLAG_DEFAULT_AUTO_COMMIT.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_INITIAL_SIZE.toUpperCase()) != null) {
+                        dsEntity.initialSize = Integer.valueOf(res.get(ConfigContainer.FLAG_INITIAL_SIZE.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_MAX_ACTIVE.toUpperCase()) != null) {
+                        dsEntity.maxActive = Integer.valueOf(res.get(ConfigContainer.FLAG_MAX_ACTIVE.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_MIN_IDLE.toUpperCase()) != null) {
+                        dsEntity.minIdle = Integer.valueOf(res.get(ConfigContainer.FLAG_MIN_IDLE.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_MAX_IDLE.toUpperCase()) != null) {
+                        dsEntity.maxIdle = Integer.valueOf(res.get(ConfigContainer.FLAG_MAX_IDLE.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_MAX_WAIT.toUpperCase()) != null) {
+                        dsEntity.maxWait = Integer.valueOf(res.get(ConfigContainer.FLAG_MAX_WAIT.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_REMOVE_ABANDONED.toUpperCase()) != null) {
+                        dsEntity.removeAbandoned = Boolean.valueOf(res.get(ConfigContainer.FLAG_REMOVE_ABANDONED.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_REMOVE_ABANDONED_TIMEOUT.toUpperCase()) != null) {
+                        dsEntity.removeAbandonedTimeoutMillis = Integer.valueOf(res.get(ConfigContainer.FLAG_REMOVE_ABANDONED_TIMEOUT.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_TIME_BETWEEN_EVICTION_RUMS.toUpperCase()) != null) {
+                        dsEntity.timeBetweenEvictionRunsMillis = Integer.valueOf(res.get(ConfigContainer.FLAG_TIME_BETWEEN_EVICTION_RUMS.toUpperCase()).toString());
+                    }
+                    if (res.get(ConfigContainer.FLAG_MIN_EVICTABLE_IDLE_TIME.toUpperCase()) != null) {
+                        dsEntity.minEvictableIdleTimeMillis = Integer.valueOf(res.get(ConfigContainer.FLAG_MIN_EVICTABLE_IDLE_TIME.toUpperCase()).toString());
+                    }
                     MULTI_DS_ENTITY.put(dsEntity.flag, dsEntity);
                     loadPool(dsEntity);
                 }
